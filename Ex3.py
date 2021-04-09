e8ff966283dcc659ccb35682980b69d72c75f026
João Sampaio <61290994+jpsampaio@users.noreply.github.com>
2021-04-08 23:11:04 -0300

    Create Ex3.py

---
+++ Ex3.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um valor, por favor: '))
+n2 = int(input('Digite outro valor para obter uma soma, por favor: '))
+s = n1 + n2
+print('Obrigado por ter chegado até aqui, a soma entre {} e {} é igual a {}'.format(n1, n2, s))

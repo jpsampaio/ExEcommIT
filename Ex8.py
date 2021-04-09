baf23ae6547fcfa199ff98564954ed6dda8653c4
João Sampaio <61290994+jpsampaio@users.noreply.github.com>
2021-04-08 23:47:21 -0300

    Create Ex8.py

---
+++ Ex8.py
@@ -0,0 +1,2 @@
+m = float(input('Uma distancia em metros: '))
+print('A medida de {} corresponde a \n {} km \n {} hm \n {} dam \n {} dm \n {} cm \n {} mm '.format(m,m/1000,m/100,m/10,m*10,m*100,m*1000))

e86d4bd42b50dc75515b12409d8b2ca0b5f34e55
João Sampaio <61290994+jpsampaio@users.noreply.github.com>
2021-04-09 01:01:10 -0300

    Create Ex27.py

---
+++ Ex27.py
@@ -0,0 +1,6 @@
+n = str(input('Digite seu nome completo: ')).strip().title().split()
+print('Muito prazer em te conhecer!\nSeu primeiro nome é {};\nSeu último sobrenome é {}'.format(n[0], n[-1]))
+
+
+
+

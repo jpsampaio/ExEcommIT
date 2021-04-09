511cebf3dc4dda75785607ce2d763ec72e08556e
João Sampaio <61290994+jpsampaio@users.noreply.github.com>
2021-04-08 23:55:01 -0300

    Create Ex9.py

---
+++ Ex9.py
@@ -0,0 +1,13 @@
+n = int(input('Digite um número:'))
+n1= 1*n
+n2= 2*n
+n3= 3*n
+n4= 4*n
+n5= 5*n
+n6= 6*n
+n7= 7*n
+n8= 8*n
+n9= 9*n
+n10= 10*n
+print ('Tabuada do {}'.format(n))
+print ('\n{} x 1 ={} \n{} x 2 ={} \n{} x 3 ={} \n{} x 4 ={} \n{} x 5 ={} \n{} x 6 ={} \n{} x 7 ={} \n{} x 8 ={} \n{} x 9 ={} \n{} x 10={}'.format(n,n1,n,n2,n,n3,n,n4,n,n5,n,n6,n,n7,n,n8,n,n9,n,n10))
